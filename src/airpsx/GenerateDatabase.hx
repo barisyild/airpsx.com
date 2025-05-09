b0cc8cc4895e86f13317e17df2b0a1d5e67afd27
barisyild <fb_bcy@hotmail.com>
2025-05-10 02:09:05 +0300

    update

---
+++ src/airpsx/GenerateDatabase.hx
@@ -0,0 +1,29 @@
+package airpsx;
+import sys.db.Sqlite;
+import sys.db.Connection;
+import sys.FileSystem;
+class GenerateDatabase {
+    static public function main() {
+        final databasePath:String = "./deploy/database.db";
+
+        FileSystem.deleteFile(databasePath);
+
+        var connection:Connection = Sqlite.open(databasePath);
+        connection.request("CREATE TABLE scripts (key String, name String, type String, minFirmware String, maxFirmware String, version String)");
+
+        var scriptNames:Array<String> = [for(scriptName in sys.FileSystem.readDirectory("scripts")) scriptName];
+        for(scriptName in scriptNames) {
+            var script:{name:String, type:String, minFirmware:String, maxFirmware:String, version:String} = haxe.Json.parse(sys.io.File.getContent('./scripts/${scriptName}/${scriptName}.json'));
+            connection.request('INSERT INTO scripts (key, name, type, minFirmware, maxFirmware, version) VALUES (${quote(connection, scriptName)}, ${quote(connection, script.name)}, ${quote(connection, script.type)}, ${quote(connection, script.minFirmware)}, ${quote(connection, script.maxFirmware)}, ${quote(connection, script.version)})');
+        }
+
+        connection.close();
+    }
+
+    private static function quote(connection:Connection, value:Dynamic):String
+    {
+        var stringBuf:StringBuf = new StringBuf();
+        connection.addValue(stringBuf, value);
+        return stringBuf.toString();
+    }
+}
