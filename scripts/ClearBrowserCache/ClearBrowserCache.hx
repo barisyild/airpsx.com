da764d0f5cf4e24bc4f867ad22382eda43e1a7f2
barisyild <fb_bcy@hotmail.com>
2025-05-13 17:01:24 +0300

    update

--- scripts/ClearBrowserCache/ClearBrowserCache.hx
+++ scripts/ClearBrowserCache/ClearBrowserCache.hx
@@ -1,28 +1,31 @@
 import airpsx.utils.FileUtils;
+import sys.FileSystem;
 
-var paths:Array<String> = [];
-var profileIds:Array<String> = FileSystem.readDirectory('/user/home');
+function main() {
+    var paths:Array<String> = [];
+    var profileIds:Array<String> = FileSystem.readDirectory('/user/home');
 
-for(profileId in profileIds)
-{
-	for(filePath in FileUtils.getRecursiveFiles(['/user/home/${profileId}/webkit/shell/']))
-	{
-		if(FileSystem.isDirectory(filePath) || !FileSystem.exists(filePath))
-			continue;
-		paths.push(filePath);
-	}
-}
+    for(profileId in profileIds)
+    {
+        for(filePath in FileUtils.getRecursiveFiles(['/user/home/${profileId}/webkit/shell/']))
+        {
+            if(FileSystem.isDirectory(filePath) || !FileSystem.exists(filePath))
+                continue;
+            paths.push(filePath);
+        }
+    }
 
-for(path in paths)
-{
-	FileSystem.deleteFile(path);
-}
+    for(path in paths)
+    {
+        FileSystem.deleteFile(path);
+    }
 
-var deletedFileCount:Int = paths.length;
-if(deletedFileCount == 0) {
-	sceKernelSendNotificationRequest('Browser cache is already empty.');
-	return 'Browser cache is already empty.';
-}else{
-	sceKernelSendNotificationRequest('Browser cache cleared successfully.');
-	return 'The following ${paths.length} files were successfully deleted; \n${paths.join("\n")}';
-}
+    var deletedFileCount:Int = paths.length;
+    if(deletedFileCount == 0) {
+        sceKernelSendNotificationRequest('Browser cache is already empty.');
+        return 'Browser cache is already empty.';
+    }else{
+        sceKernelSendNotificationRequest('Browser cache cleared successfully.');
+        return 'The following ${paths.length} files were successfully deleted; \n${paths.join("\n")}';
+    }
+}();
